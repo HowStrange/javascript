f37425adab3a8d3e74774ac529ad712f5fa3f7df
alansilva <howstrangetec@gmail.com>
2021-07-09 17:34:14 -0300

    Testando javascript

---
+++ helloworld/helloworld.js
@@ -0,0 +1,11 @@
+console.log("Hello Wolrd, Estou aprendendo JavaScript!")
+
+let nome = 'Alan'
+let idade = 19
+let solteiro = true
+let dataNascimento = 6.9
+let test = null
+
+window.alert("Janela de Alerta")
+window.confirm("Janela com confirmação")
+window.prompt("Janela que recebe dados")
